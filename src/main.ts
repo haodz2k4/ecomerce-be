6bce34a053496512765775232906ab2a16a9512c
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-01-30 07:52:24 +0700

    Set up versioning

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
   //Transform interceptor 
   const reflector = app.get(Reflector)
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
+  //Versioning
+  app.setGlobalPrefix('api')
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  })
   const port = configService.get<string>('PORT');
   await app.listen(port);
 }
