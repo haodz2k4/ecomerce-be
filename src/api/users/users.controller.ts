2ec2e8bbd131c3cb0d92dd7630bdce4e66f20882
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-02 20:56:03 +0700

    Response message user

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -1,36 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { QueryUserDto } from './dto/query-user.dto';
 import { UserResDto } from './dto/user-res.dto';
 import { PaginatedResDto } from 'src/common/dto/paginated-res.dto';
+import { ResponseMessage } from 'src/decorator/response-message.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ResponseMessage('Create user successfully')
   create(@Body() createUserDto: CreateUserDto) :Promise<UserResDto> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ResponseMessage('Get users successfully')
   findAll(@Query() queryUserDto: QueryUserDto) :Promise<PaginatedResDto<UserResDto>> {
     return this.usersService.findAll(queryUserDto);
   }
 
   @Get(':id')
+  @ResponseMessage('Get user by id successfully')
   findOne(@Param('id') id: string): Promise<UserResDto> {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ResponseMessage('Update user successfully')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) :Promise<UserResDto> {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ResponseMessage('Remove user successfully')
   remove(@Param('id') id: string): Promise<void> {
     return this.usersService.remove(id);
   }
