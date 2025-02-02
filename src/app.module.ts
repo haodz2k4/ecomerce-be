9917a584ccbd6b6321e43f19aac265799a062afb
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-02 21:00:38 +0700

    Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './api/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './api/auth/auth.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { PrismaModule } from './prisma/prisma.module';
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
