8da922667b5f34afb362acc42c6ab47b7a97dd18
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-11 10:48:55 +0700

    update other

--- src/api/permissions/permissions.controller.ts
+++ src/api/permissions/permissions.controller.ts
@@ -1,36 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpCode, HttpStatus } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
 import { PermissionResDto } from './dto/permission-res.dto';
 import { QueryPermissionDto } from './dto/query-permission.dto';
 import { PaginatedResDto } from 'src/common/dto/paginated-res.dto';
+import { ResponseMessage } from 'src/decorator/response-message.decorator';
 
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
   @Post()
+  @ResponseMessage('Create permission successfully')
   create(@Body() createPermissionDto: CreatePermissionDto) :Promise<PermissionResDto> {
     return this.permissionsService.create(createPermissionDto);
   }
 
   @Get()
+  @ResponseMessage('Get permissions')
   findAll(@Query() queryPermissionDto: QueryPermissionDto) :Promise<PaginatedResDto<PermissionResDto>> {
     return this.permissionsService.findAll(queryPermissionDto);
   }
 
   @Get(':id')
+  @ResponseMessage('Get permission')
   findOne(@Param('id') id: string) :Promise<PermissionResDto> {
     return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
+  @ResponseMessage('Update permission')
   update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto): Promise<PermissionResDto> {
     return this.permissionsService.update(id, updatePermissionDto);
   }
 
   @Delete(':id')
+  @ResponseMessage('Remove permission')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id: string) :Promise<void> {
     return this.permissionsService.remove(id);
   }
--- src/api/roles/roles.controller.ts
+++ src/api/roles/roles.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpCode, HttpStatus } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
@@ -36,6 +36,7 @@ export class RolesController {
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ResponseMessage('Remove role successfully')
   remove(@Param('id') id: string) :Promise<void> {
     return this.rolesService.remove(id);
