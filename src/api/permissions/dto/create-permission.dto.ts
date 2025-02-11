7b80c6458cfafca9ed91a3aa1a32dcbd67c1b98f
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-11 08:35:34 +0700

    Roles permission model

--- src/api/permissions/dto/create-permission.dto.ts
+++ src/api/permissions/dto/create-permission.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePermissionDto {}
+import { IsEnum, IsString } from "class-validator";
+import { PermissionNameEnum } from "src/constants/entity.constant";
+
+
+export class CreatePermissionDto {
+
+    @IsEnum(PermissionNameEnum)
+    name: PermissionNameEnum;
+
+    @IsString()
+    resource: string;
+}
