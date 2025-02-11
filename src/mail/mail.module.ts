81537357f869a154253484a82cfad4ca8dabc537
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-11 11:13:19 +0700

    Config mail module and confirmation hbs template

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  providers: [MailService]
+  imports: [MailerModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => (
+      {
+        transport: {
+          auth:{
+            user: configService.get<string>('SMTP_USERNAME'),
+            pass: configService.get<string>('SMTP_PASS')
+          }
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true
+          }
+        }
+      }
+    )
+  })],
+  providers: [MailService],
+  exports: [MailService]
 })
 export class MailModule {}
