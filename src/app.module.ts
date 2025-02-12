c8e2f595f8518066a95788d8f4da89a4cde640bd
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-12 08:19:41 +0700

    Connected to bull mq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './api/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './api/auth/auth.module';
 import { RolesModule } from './api/roles/roles.module';
 import { PermissionsModule } from './api/permissions/permissions.module';
 import { MailModule } from './mail/mail.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -17,7 +18,18 @@ import { MailModule } from './mail/mail.module';
     AuthModule,
     RolesModule,
     PermissionsModule,
-    MailModule
+    MailModule,
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+          username: configService.get<string>('REDIS_USERNAME'),
+          password: configService.get<string>('REDIS_PASSWORD')
+        }
+      })
+    })
   ],
   controllers: [],
   providers: [],
