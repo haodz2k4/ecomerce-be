d7ccd2adeb915c27e5ebb7ab4befb5e38c2bc42a
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2025-02-13 10:20:11 +0700

    Upload Single and upload image

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { UploadApiErrorResponse, UploadApiResponse, v2 } from 'cloudinary';
+import toStream from "buffer-to-stream";
 @Injectable()
-export class CloudinaryService {}
+
+export class CloudinaryService {
+
+    async uploadSingle( file: Express.Multer.File ): Promise<UploadApiResponse | UploadApiErrorResponse> {
+        
+        return new Promise((resolve, reject) => {
+            const upload = v2.uploader.upload_stream((error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            });
+        
+            toStream(file.buffer).pipe(upload);
+        });
+    }
+
+    async uploadMulti(file: Express.Multer.File[]) :Promise<(UploadApiResponse | UploadApiErrorResponse)[]> {
+        return await Promise.all(file.map((item) => this.uploadSingle(item)))
+    }
+}
